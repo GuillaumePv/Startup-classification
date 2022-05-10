4471cb6f0d6ef257e4b25ac3dc2483fdf0d52778
Guillaume Pavé <guillaumepave@gmail.com>
2022-05-10 18:26:47 +0200

    Update check_github_file.py

--- download/check_github_file.py
+++ download/check_github_file.py
@@ -1,3 +1,4 @@
+import shutil
 from tqdm import tqdm
 import requests
 import pandas as pd
@@ -5,6 +6,11 @@ from bs4 import BeautifulSoup
 import os
 import sys
 import time
+import shutil
+
+# Libraries in order to paralel code
+from joblib import Parallel, delayed
+import multiprocessing
 #adding directory to path
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -19,6 +25,7 @@ class Github:
         self.read_dir = read_dir
         self.df_links = None
         self.read_links()
+        self.num_cores = multiprocessing.cpu_count()
     
     def read_links(self):
         self.df_links = pd.read_csv(self.read_dir + "internal_repo_actitivity_detail.csv")
@@ -29,7 +36,7 @@ class Github:
         list_file = os.listdir(self.save_dir)
         error = 0
         # check file aleardy done
-        for file in tqdm(list_file):
+        def func_check(file,copy_good=False):
             f = open(self.save_dir+file, encoding="utf-8")
             bug = False
             for line in f.readlines():
@@ -37,21 +44,21 @@ class Github:
                     f_write = open("./download/github_too_many_requests.txt","a")
                     f_write.write(file+","+line+",\n")
                     f_write.close()
-                    error += 1
+                    # error += 1
                     bug = True
-                    # print(line)
+                        # print(line)
                 elif line == "400: Invalid request":
                     f_write = open("./download/github_invalid_request.txt","a")
                     f_write.write(file+","+line+",\n")
                     f_write.close()
-                    error += 1
+                    # error += 1
                     bug = True
-                    # print(line)
+                        # print(line)
                 elif line == "404: Not Found":
                     f_write = open("./download/github_not_found.txt","a")
                     f_write.write(file+","+line+",\n")
                     f_write.close()
-                    error += 1
+                    # error += 1
                     bug = True
             if bug == False:
                 login = file.split("_")[0]
@@ -59,9 +66,16 @@ class Github:
                 f_write = open("./download/github_done.txt","a")
                 f_write.write(file+","+login+","+repo+",\n")
                 f_write.close()
+                if copy_good:
+                    dest_path = f"{path_github_folder}good_readme/{file}"
+                    shutil.copyfile(self.save_dir+file, dest_path)
 
-            
+                
             f.close()
+
+        # Parallel(n_jobs=self.num_cores)(delayed(func_check)(i) for i in tqdm(list_file))
+        for file in tqdm(list_file):
+            func_check(file)
         
         print("Number of error: ",error)
         print("% error: ", error/len(list_file))
